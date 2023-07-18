2628675ac32d2568cf88a45640457a76fef67ba8
edilbek1507 <edilbek555@gmail.com>
2023-07-18 15:53:47 +0600

    seconnd-commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MovieItem from "../components/MovieItem";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <MovieItem />
+    </div>
+  );
 };
 
 export default HomePage;
