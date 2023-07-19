ac309158a89875df0c75c583bf1b5424b09fb482
Shorecut <chermanderco@gmail.com>
2023-07-19 17:03:55 +0600

    sort2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,22 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useMovieContext } from "../context/MovieContext";
 import MovieItem from "./MovieItem";
 import { Button } from "react-bootstrap";
 
 const MovieList = () => {
   const { movies, getMovies } = useMovieContext();
+  const [sortedMovies, setSortedMovies] = useState(movies);
+  const [minMovies, setMinMovies] = useState(movies);
 
   useEffect(() => {
     getMovies();
   }, []);
 
-  const sort = movies.sort((a, b) => (+a.stars > +b.stars ? -1 : 1));
+  function sort_plus() {
+    movies.sort((a, b) => (+a.stars > +b.stars ? -1 : 1));
+    setSortedMovies(movies);
+  }
+  function sort_minus() {
+    movies.sort((a, b) => (+a.stars < +b.stars ? -1 : 1));
+    setMinMovies(movies);
+  }
+
+  //const sort = movies.sort((a, b) => (+a.stars > +b.stars ? -1 : 1));
 
   return (
-    <div className="movieList">
-      {sort.map((item) => (
-        <MovieItem key={item.id} item={item} />
-      ))}
+    <div>
+      <div className="sort-btn">
+        <Button
+          onClick={sort_plus}
+          className="first_button"
+          variant="outline-light"
+        >
+          Popular
+        </Button>
+        <Button onClick={sort_minus} variant="outline-light">
+          Low Rating
+        </Button>
+      </div>
+      <div className="movieList">
+        {movies.map((item) => (
+          <MovieItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
